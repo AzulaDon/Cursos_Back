dbbf014e00fe4c308d9ff16fdbbfeff582f1a08e
Armando <armanditomart221205@gmail.com>
2026-04-20 19:02:25 -0600

    Carga de datos

---
+++ src/main/java/com/curso/OmetepecGuerrero/OmetepecGuerreroApplication.java
@@ -0,0 +1,13 @@
+package com.curso.OmetepecGuerrero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmetepecGuerreroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmetepecGuerreroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/curso/OmetepecGuerrero/OmetepecGuerreroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.OmetepecGuerrero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmetepecGuerreroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
